15346d3d20ec2dfcdbe5bb4b80f1d5fc59193bf9
wanda prasetya <107533381+wandaprasetya23@users.noreply.github.com>
2022-06-26 08:59:27 +0700

    Update config.js
    Signed-off-by: wanda prasetya <107533381+wandaprasetya23@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,14 +37,14 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
-global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
+global.owner = ['628975217200']
+global.premium = ['628975217200']
+global.botname = 'SANS BOT'
+global.ownername = 'wanda prasetya'
+global.group1 = 'https://chat.whatsapp.com/EtVBin6QxepAU4esPGHsne'
+global.group2 = 'https://chat.whatsapp.com/EtVBin6QxepAU4esPGHsne'
+global.packname = 'sans bot'
+global.ytchannel = 'https://youtube.com/shorts/OZldu-te-TU?feature=share'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
